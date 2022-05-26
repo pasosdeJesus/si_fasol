87cc3fde7e12a789872671fee873add288ce88aa
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-05-26 09:55:29 -0500

    Observaciones. Closes #19

---
+++ app/controllers/sip/observaciones_controller.rb
@@ -0,0 +1,49 @@
+module Sip
+  class ObservacionesController < Heb412Gen::ModelosController
+    before_action :set_observacion,
+      only: [:show, :edit, :update, :destroy]
+    load_and_authorize_resource  class: ::Observacion
+
+    def clase 
+      "Sip::Observacion"
+    end
+
+    def set_observacion
+      @basica = Sip::Observacion.find(params[:id])
+    end
+
+    def atributos_index
+      [
+        :id, 
+        :usuario_id, 
+        :fecha,
+        :observacion
+        #:estadoobs_id,
+        #:usuarionotificar_ids
+      ]
+    end
+
+    def atributos_form
+      [
+        :id, 
+        :usuario_id, 
+        :fecha,
+        :observacion
+        #:estadoobs_id,
+        #:usuarionotificar_ids
+      ]
+    end
+
+    def genclase
+      'F'
+    end
+
+    def params_lista
+      atributos_form
+    end
+
+    def observacion_params
+      params.require(:observacion).permit(params_lista)
+    end
+
+  end
--- app/models/sivel2_gen/caso.rb
+++ app/models/sivel2_gen/caso.rb
@@ -5,6 +5,17 @@ module Sivel2Gen
   class Caso < ActiveRecord::Base
     include Sivel2Gen::Concerns::Models::Caso
 
+    has_many :caso_observacion, dependent: :delete_all,
+      class_name: 'Sivel2Gen::CasoObservacion',
+      foreign_key: 'caso_id'
+    has_many :observacion, through: :caso_observacion, 
+      dependent: :delete_all,
+      class_name: 'Sip::Observacion'
+    accepts_nested_attributes_for :observacion,
+      allow_destroy: true, reject_if: :all_blank
+    accepts_nested_attributes_for :caso_observacion,
+      allow_destroy: true, reject_if: :all_blank
+
     validates :ayudafasol, length: {maximum: 1024}
     validates :codigofasol, length: {maximum: 32}
     validates :marbetefasol, length: {maximum: 256}
