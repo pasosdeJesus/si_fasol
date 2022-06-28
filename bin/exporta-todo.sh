c28af23d3d351d8075f35c4d07d2f91fc4ab42b7
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-06-28 10:43:13 -0500

    Refina produccion

--- bin/exporta-todo.sh
+++ bin/exporta-todo.sh
@@ -13,8 +13,10 @@ echo "Por generar datos de casos para enviar a "\
   "$SIVEL2_EXP_USUARIO@$SIVEL2_EXP_MAQ:$SIVEL2_EXP_DIR "\
   "con opciones '$SIVEL2_EXP_OPSCP'";
 echo "Esto puede cambiarse en .env";
-echo "[ENTER] para continuar o [Ctrl]-[C] para detener";
-read a
+if (test "$DERECHO" != "1") then {
+  echo "[ENTER] para continuar o [Ctrl]-[C] para detener";
+  read a
+} fi;
 if (test "$RAILS_ENV" = "development") then {
   b=$BD_DES
 } else {
@@ -28,19 +30,18 @@ function eval_con_eco {
 
 echo "1 de 3. Genera volcado ..."
 ord="pg_dump $b --no-owner --clean -h /var/www/var/run/postgresql -U $BD_USUARIO --no-owner"
-eval_con_eco "pg_dump $b --no-owner --clean -h /var/www/var/run/postgresql -U
-$BD_USUARIO --no-owner | grep -v 'DROP TABLE public.usuario;' | grep -v 'DROP SEQUENCE public.usuario_id_seq;' > $SIP_RUTA_VOLCADOS/sivel2-todo-$dm.sql"
+eval_con_eco "pg_dump $b --no-owner --clean -h /var/www/var/run/postgresql -U $BD_USUARIO --no-owner | grep -v 'DROP TABLE public.usuario;' | grep -v 'DROP SEQUENCE public.usuario_id_seq;' > $SIP_RUTA_VOLCADOS/${b}-todo-$dm.sql"
 #eval $ord > $SIP_RUTA_VOLCADOS/sivel2-todo-$dm.sql #Todo por procesar mas
 
 echo "2 de 3. Comprime ..."
-eval_con_eco "gzip $SIP_RUTA_VOLCADOS/sivel2-todo-$dm.sql"
+eval_con_eco "gzip $SIP_RUTA_VOLCADOS/${b}-todo-$dm.sql"
 
 if (test "$SIVEL2_EXP_USUARIO" != "") then {
   echo "3 de 3. Envía ..."
   if (test "$SIVEL2_EXP_MAQ" = "localhost" -o "$SIVEL2_EXP_MAQ" = "127.0.0.1") then {
-    eval_con_eco "cp $SIP_RUTA_VOLCADOS/sivel2-todo-$dm.sql.gz $SIVEL2_EXP_DIR "
+    eval_con_eco "cp $SIP_RUTA_VOLCADOS/${b}-todo-$dm.sql.gz $SIVEL2_EXP_DIR "
   } else {
-    eval_con_eco "scp $SIVEL2_EXP_OPSCP $SIP_RUTA_VOLCADOS/sivel2-todo-$dm.sql.gz $SIVEL2_EXP_USUARIO@$SIVEL2_EXP_MAQ:$SIVEL2_EXP_DIR "
+    eval_con_eco "scp $SIVEL2_EXP_OPSCP $SIP_RUTA_VOLCADOS/${b}-todo-$dm.sql.gz $SIVEL2_EXP_USUARIO@$SIVEL2_EXP_MAQ:$SIVEL2_EXP_DIR "
   } fi;
 } fi;
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
 
 
   rutarel = ENV.fetch('RUTA_RELATIVA', 'sivel2/')
-  scope rutarel do 
+  scope rutarel do
 
     devise_scope :usuario do
       get 'sign_out' => 'devise/sessions#destroy'
 
       # El siguiente para superar mala generación del action en el formulario
-      # cuando se monta en sitio diferente a / y se autentica mal (genera 
+      # cuando se monta en sitio diferente a / y se autentica mal (genera
       # /puntomontaje/puntomontaje/usuarios/sign_in )
-      if (Rails.configuration.relative_url_root != '/') 
-        ruta = File.join(Rails.configuration.relative_url_root, 
+      if (Rails.configuration.relative_url_root != '/')
+        ruta = File.join(Rails.configuration.relative_url_root,
                          'usuarios/sign_in')
         post ruta, to: 'devise/sessions#create'
         get  ruta, to: 'devise/sessions#new'
@@ -20,12 +20,12 @@ Rails.application.routes.draw do
 
     devise_for :usuarios, :skip => [:registrations], module: :devise
     as :usuario do
-      get 'usuarios/edit' => 'devise/registrations#edit', 
-        :as => 'editar_registro_usuario'    
-      put 'usuarios/:id' => 'devise/registrations#update', 
-        :as => 'registro_usuario'            
+      get 'usuarios/edit' => 'devise/registrations#edit',
+        :as => 'editar_registro_usuario'
+      put 'usuarios/:id' => 'devise/registrations#update',
+        :as => 'registro_usuario'
     end
-    resources :usuarios, path_names: { new: 'nuevo', edit: 'edita' } 
+    resources :usuarios, path_names: { new: 'nuevo', edit: 'edita' }
 
     get '/casos/mapaosm' => 'sivel2_gen/casos#mapaosm'
 
@@ -33,6 +33,17 @@ Rails.application.routes.draw do
 
     get '/orgsociales/jerarquia' => 'sip/orgsociales#jerarquia',
       as: :jerarquia_orgsociales
+
+    namespace :admin do
+      ab = ::Ability.new
+      ab.tablasbasicas.each do |t|
+        if (t[0] == "")
+          c = t[1].pluralize
+          resources c.to_sym,
+            path_names: { new: 'nueva', edit: 'edita' }
+        end
+      end
+    end
 
     root 'sivel2_gen/hogar#index'
   end # scope
