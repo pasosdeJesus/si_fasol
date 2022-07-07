b33f4fcbfe8de816cc0883b81663603dfb30bde6
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-07-07 08:54:04 -0500

    Replantea consultas

--- app/controllers/sivel2_gen/conteos_controller.rb
+++ app/controllers/sivel2_gen/conteos_controller.rb
@@ -9,23 +9,54 @@ module Sivel2Gen
     load_and_authorize_resource class: Sivel2Gen::Caso
 
     def fasol_banco
-      pFini = '2010-01-01'
-      pFfin = ''
-      pTviolencia = ''
-      pEtiqueta1 = ''
-      pEtiqueta2 = ''
-      pExcluirCateRep = ''
-      pSegun = ''
-      pDepartamento = ''
-      pMunicipio = ''
-      pCategoria = Sivel2Gen::Categoria.habilitados.pluck(:id)
+      sub = "SELECT DISTINCT caso.id AS caso_id, "\
+        " id_categoria, ac.id_persona, cat.id_pconsolidado, "\
+        " p.nombre AS rotulo "\
+        " FROM sivel2_gen_caso AS caso"\
+        " JOIN sivel2_gen_acto AS ac ON caso.id=ac.id_caso"\
+        " JOIN sivel2_gen_categoria AS cat "\
+        "  ON ac.id_categoria=cat.id "\
+        " JOIN sivel2_gen_pconsolidado AS p "\
+        "  ON p.id=cat.id_pconsolidado "
 
-      @opsegun, @coltotales, @totalesfila, @numpersonas, @enctabla, 
-        @cuerpotabla = Sivel2Gen::ConteosController::victimizaciones_gen_tabla(
-          pFini, pFfin, pTviolencia, pEtiqueta1, pEtiqueta2,
-          "0", pSegun, "0", "0",
-          nil, pCategoria, 'RÓTULO'
-      )
+      r1 = ActiveRecord::Base.connection.select_all(
+        "SELECT COUNT(DISTINCT id_persona) FROM (#{sub}) AS sub")
+      @numpersonas = r1[0]['count']
+
+      q4 = "SELECT rotulo, count(*) FROM (#{sub}) AS sub "\
+        " GROUP BY 1 ORDER BY 1"
+
+      puts "OJO q4=#{q4}"
+      r = ActiveRecord::Base.connection.select_all(q4) 
+      fila = 'rotulo'
+      enctabla = []
+      inter = {}
+      filas = []
+      et = ['Acciones violatorias']
+      r.each do |t|
+        if !inter[t[fila]]
+          inter[t[fila]] = {}
+        end
+        inter[t[fila]] = t['count'].to_i
+        if !filas.include?(t[fila].to_s)
+          filas << t[fila].to_s
+        end
+      end
+      enctabla = et
+
+      i = 1
+      cuerpotabla = []
+      filas.each do |f|
+        ft = [f]
+        et.each do |e|
+          ft << (inter[f] ? inter[f] : 0)
+        end
+        cuerpotabla << ft
+      end
+
+      @enctabla = enctabla
+      @cuerpotabla = cuerpotabla
+
       respond_to do |format|
         format.html { render 'fasol_banco', layout: false}
         format.json { head :no_content }
@@ -33,23 +64,64 @@ module Sivel2Gen
     end
 
     def fasol_banco_regionales
-      pFini = '2010-01-01'
-      pFfin = ''
-      pTviolencia = ''
-      pEtiqueta1 = ''
-      pEtiqueta2 = ''
-      pExcluirCateRep = ''
-      pSegun = ''
-      pDepartamento = ''
-      pMunicipio = ''
-      pCategoria = Sivel2Gen::Categoria.habilitados.pluck(:id)
+      sub = "SELECT DISTINCT caso.id AS caso_id, "\
+        " id_categoria, ac.id_persona, cat.id_pconsolidado, "\
+        " p.nombre AS rotulo, "\
+        "(SELECT r.nombre FROM  sivel2_gen_caso_region AS cr "\
+        "  JOIN sivel2_gen_region AS r ON cr.id_region=r.id "\
+        "  WHERE cr.id_caso=caso.id LIMIT 1) AS region "\
+        " FROM sivel2_gen_caso AS caso"\
+        " JOIN sivel2_gen_acto AS ac ON caso.id=ac.id_caso"\
+        " JOIN sivel2_gen_categoria AS cat "\
+        "  ON ac.id_categoria=cat.id "\
+        " JOIN sivel2_gen_pconsolidado AS p "\
+        "  ON p.id=cat.id_pconsolidado "
 
-      @opsegun, @coltotales, @totalesfila, @numpersonas, @enctabla, 
-        @cuerpotabla = Sivel2Gen::ConteosController::victimizaciones_gen_tabla(
-          pFini, pFfin, pTviolencia, pEtiqueta1, pEtiqueta2,
-          "0", 'REGIÓN', "0", "0",
-          nil, pCategoria, 'RÓTULO'
-      )
+      q4 = "SELECT rotulo, region, count(*) FROM (#{sub}) AS sub "\
+        " GROUP BY 1, 2 ORDER BY 1,2"
+
+      puts "OJO q4=#{q4}"
+      r = ActiveRecord::Base.connection.select_all(q4) 
+      fila = 'rotulo'
+      columna = 'region'
+      enctabla = []
+      inter = {}
+      filas = []
+      et = []
+      r.each do |t|
+        if !inter[t[fila]]
+          inter[t[fila]] = {}
+        end
+        if !inter[t[fila]][t[columna]]
+          inter[t[fila]][t[columna]] = {}
+        end
+        inter[t[fila]][t[columna]] = t['count'].to_i
+        if !et.include?(t[columna].to_s)
+          et << t[columna].to_s
+        end
+        if !filas.include?(t[fila].to_s)
+          filas << t[fila].to_s
+        end
+      end
+      et.sort!
+      enctabla << 'Hecho de violencia'
+
+      i = 1
+      et.each do |e|
+        enctabla << CGI.escapeHTML(e)
+        i += 1
+      end
+      cuerpotabla = []
+      filas.each do |f|
+        ft = [f]
+        et.each do |e|
+          ft << (inter[f] && inter[f][e] ? inter[f][e] : 0)
+        end
+        cuerpotabla << ft
+      end
+
+      @enctabla = enctabla
+      @cuerpotabla = cuerpotabla
 
       respond_to do |format|
         format.html { render 'fasol_banco_regionales', layout: false}
