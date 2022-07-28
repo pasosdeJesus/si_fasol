dd4370f4675dac95db3ec4779e60aef9adf53a7e
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-07-28 10:52:46 -0500

    Script para importar de un volcado todo y eliminar lo privado para dejar lo publico

---
+++ bin/importapublico.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+. .env
+
+
+function eval_con_eco {
+  echo $@
+  eval $@
+}
+
+v=$1
+if (test "$v" = "-reciente") then {
+	v1=`ls -lat ${SIP_RUTA_VOLCADOS} | grep "fasol_prod-todo.*sql.gz" | head -n 1 | sed -e "s/.* fasol_prod/fasol_prod/g"`
+  v2="${SIP_RUTA_VOLCADOS}/$v1"
+  echo "Volcado más reciente es $v2"
+  cp $v2 /tmp/$v1
+  eval_con_eco "gunzip -q /tmp/$v1"
+  v3=`echo $v1 | sed -e "s/\.gz//g"`
+  v="/tmp/$v3"
+  echo "Tras descomprimir se usará $v"
+} fi;
+if (test ! -f "$v") then {
+  echo "Primer parámetro ($v) debe ser -reciente o volcado por importar"
+  exit 1;
+} fi;
+
+if (test $RAILS_ENV = "production") then {
+  BD=$BD_PRO
+  rails=bin/railsp
+} else {
+  BD=$BD_DES
+  rails=bin/rails
+} fi;
+
+eval_con_eco "dropdb -h /var/www/var/run/postgresql -U $BD_USUARIO $BD"
+eval_con_eco "createdb -h /var/www/var/run/postgresql -U $BD_USUARIO $BD"
+eval_con_eco "psql -h /var/www/var/run/postgresql -U $BD_USUARIO $BD -f $v"
+
+$rails runner -e $RAILS_ENV scripts/elimina_datos_privados.rb 
