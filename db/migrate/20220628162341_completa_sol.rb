fa464a18f151c572c0f8d7d52079622dba45725d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-08-03 08:36:00 -0500

    Actualiza

--- db/migrate/20220628162341_completa_sol.rb
+++ db/migrate/20220628162341_completa_sol.rb
@@ -1,5 +1,7 @@
 class CompletaSol < ActiveRecord::Migration[7.0]
   def change
-    add_column :sip_solicitud, :estadosol_id, :integer
+    if !column_exists?(:sip_solicitud, :estadosol_id)
+      add_column :sip_solicitud, :estadosol_id, :integer
+    end
   end
 end
