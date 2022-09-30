8f9ee6aaced577c3693dc7464cb7b92c7b90cced
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-09-30 09:11:04 -0500

    Arregla falta de vista actividades en plantillas

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,9 @@ class Ability  < Apo214::Ability
   def tablasbasicas
     r = (Sip::Ability::BASICAS_PROPIAS - 
          [['Sip', 'oficina']]
-        ) + Sivel2Gen::Ability::BASICAS_PROPIAS - [
+        ) + 
+        Heb412Gen::Ability::BASICAS_PROPIAS +
+        Sivel2Gen::Ability::BASICAS_PROPIAS - [
           ['Sivel2Gen', 'actividadoficio'],
           ['Sivel2Gen', 'escolaridad'],
           ['Sivel2Gen', 'estadocivil'],
@@ -21,6 +23,47 @@ class Ability  < Apo214::Ability
 
     return r
   end
+
+  BASICAS_ID_NOAUTO = []
+
+  def basicas_id_noauto 
+    Sip::Ability::BASICAS_ID_NOAUTO +
+      Heb412Gen::Ability::BASICAS_ID_NOAUTO +
+      Cor1440Gen::Ability::BASICAS_ID_NOAUTO +
+      Sivel2Gen::Ability::BASICAS_ID_NOAUTO +
+      BASICAS_ID_NOAUTO
+  end
+
+  NOBASICAS_INDSEQID = []
+
+  def nobasicas_indice_seq_con_id 
+    Sip::Ability::NOBASICAS_INDSEQID +
+      Heb412Gen::Ability::NOBASICAS_INDSEQID +
+      Cor1440Gen::Ability::NOBASICAS_INDSEQID +
+      Sivel2Gen::Ability::NOBASICAS_INDSEQID +
+      NOBASICAS_INDSEQID
+  end
+
+  BASICAS_PRIO = []
+
+  def tablasbasicas_prio 
+    Sip::Ability::BASICAS_PRIO +
+      Heb412Gen::Ability::BASICAS_PRIO +
+      Sivel2Gen::Ability::BASICAS_PRIO +
+      Cor1440Gen::Ability::BASICAS_PRIO +
+      BASICAS_PRIO
+  end
+
+  CAMPOS_PLANTILLAS_PROPIAS = {
+  }
+
+  def campos_plantillas
+    Heb412Gen::Ability::CAMPOS_PLANTILLAS_PROPIAS.clone.
+      merge(Cor1440Gen::Ability::CAMPOS_PLANTILLAS_PROPIAS.clone).
+      merge(Sivel2Gen::Ability::CAMPOS_PLANTILLAS_PROPIAS.clone).
+      merge(CAMPOS_PLANTILLAS_PROPIAS.clone)
+  end
+
 
   # Establece autorizaciones con CanCanCan
   def initialize(usuario = nil)
