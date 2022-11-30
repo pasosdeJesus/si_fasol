71b0b2f1adb949e8cfbb03e7b40d4a054af3ba7a
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-11-30 08:45:59 -0500

    sin opciones caracterizacion

--- db/datos-basicas.sql
+++ db/datos-basicas.sql
@@ -80,39 +80,4 @@ SELECT pg_catalog.setval('public.cargoestado_id_seq', 100, true);
 
 
 
---- OpcionesCaracterizacion
-
-
-INSERT INTO public.cor1440_gen_opcioncaracterizacion 
-(id, nombre, actividadpf_id, fechacreacion, created_at, updated_at) VALUES (
-  1, 'Atención Psicosocial', 33, '2022-10-31', '2022-10-31', '2022-10-31'
-);
-INSERT INTO public.cor1440_gen_opcioncaracterizacion 
-(id, nombre, actividadpf_id, fechacreacion, created_at, updated_at) VALUES (
-  2, 'Incidencia', 35, '2022-10-31', '2022-10-31', '2022-10-31' -- PLJTC  ActividadMLA3RedT
-);
-INSERT INTO public.cor1440_gen_opcioncaracterizacion 
-(id, nombre, actividadpf_id, fechacreacion, created_at, updated_at) VALUES (
-  3, 'Talleres de formación', 34, '2022-10-31', '2022-10-31', '2022-10-31' -- PLJTC  ActividadMLA2For
-);
-INSERT INTO public.cor1440_gen_opcioncaracterizacion 
-(id, nombre, actividadpf_id, fechacreacion, created_at, updated_at) VALUES (
-  4, 'Reclamaciones Jurídicas', 35, '2022-10-31', '2022-10-31', '2022-10-31' -- PLJTC  ActividadMLA3RedT
-);
-INSERT INTO public.cor1440_gen_opcioncaracterizacion 
-(id, nombre, actividadpf_id, fechacreacion, created_at, updated_at) VALUES (
-  5, 'Vigilancia administrativa', 35, '2022-10-31', '2022-10-31', '2022-10-31' -- PLJTC  ActividadMLA3RedT
-);
-INSERT INTO public.cor1440_gen_opcioncaracterizacion 
-(id, nombre, actividadpf_id, fechacreacion, created_at, updated_at) VALUES (
-  6, 'Acción Humanitaria', 35, '2022-10-31', '2022-10-31', '2022-10-31' -- PLJTC  ActividadMLA3RedT
-);
-INSERT INTO public.cor1440_gen_opcioncaracterizacion 
-(id, nombre, actividadpf_id, fechacreacion, created_at, updated_at) VALUES (
-  7, 'Auxilio Educativo', 35, '2022-10-31', '2022-10-31', '2022-10-31' -- PLJTC  ActividadMLA3RedT
-);
-INSERT INTO public.cor1440_gen_opcioncaracterizacion 
-(id, nombre, actividadpf_id, fechacreacion, created_at, updated_at) VALUES (
-  8, 'Auxilio Emergencia', 35, '2022-10-31', '2022-10-31', '2022-10-31' -- PLJTC  ActividadMLA3RedT
-);
 
