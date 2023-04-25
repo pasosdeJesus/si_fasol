0f289227bf2c2546f250b68d5cfe42fcf061dc40
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-04-25 11:15:29 -0500

    Mejora valores predeterminados

--- config/initializers/punto_montaje.rb
+++ config/initializers/punto_montaje.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 Sivel2::Application.config.relative_url_root = ENV.fetch(
-  "RUTA_RELATIVA", "/sivel2"
+  "RUTA_RELATIVA", "/si/fasol"
 )
 Sivel2::Application.config.assets.prefix = if ENV.fetch(
-  "RUTA_RELATIVA", "/sivel2"
+  "RUTA_RELATIVA", "/si/fasol"
 ) == "/"
   "/assets"
 else
-  (ENV.fetch("RUTA_RELATIVA", "/sivel2") + "/assets")
+  (ENV.fetch("RUTA_RELATIVA", "/si/fasol") + "/assets")
 end
