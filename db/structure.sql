b55a5f418bec66573d8563a893a682e64d3effd7
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-07-12 11:52:11 -0500

    Actualiza a msip con perfiles por omision. Closes #156

--- db/structure.sql
+++ db/structure.sql
@@ -10,6 +10,13 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: public; Type: SCHEMA; Schema: -; Owner: -
+--
+
+-- *not* creating schema, since initdb creates it
+
+
+--
 -- Name: es_co_utf_8; Type: COLLATION; Schema: public; Owner: -
 --
 
@@ -12362,6 +12369,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20230620173429'),
 ('20230621020213'),
 ('20230622205529'),
-('20230622205530');
+('20230622205530'),
+('20230712163859');
 
 
