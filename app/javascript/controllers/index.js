f7b792bfe6920549308f6c3085caa5d0bfcc8f22
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-11-23 07:59:10 -0500

    Actualiza

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -16,5 +16,8 @@ application.register("msip--filtro-tan", Msip__FiltroTanController)
 import Msip__SindocautController from "./msip/sindocaut_controller"
 application.register("msip--sindocaut", Msip__SindocautController)
 
+import Sivel2Gen__EdadController from "./sivel2_gen/edad_controller"
+application.register("sivel2-gen--edad", Sivel2Gen__EdadController)
+
 import UsuarioConGrupoController from "./usuario_con_grupo_controller"
 application.register("usuario-con-grupo", UsuarioConGrupoController)
